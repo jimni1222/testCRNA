d26a27c80a5b02056af0824d7fe5543ea569c036
Jasmine/kimjimin <wlalsdlwwkd@gmail.com>
2020-04-20 10:37:07 +0900

    First commit

--- App.tsx
+++ App.tsx
@@ -1,19 +1,47 @@
+import './global';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+const Caver = require('caver-js');
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+interface States {
+  latestBlock: number
+}
+
+export default class App extends React.Component<{}, States> {
+  constructor() {
+    super({});
+    this.state = {
+      latestBlock: -1,
+    }
+  }
+
+  componentWillMount() {
+    const caver = new Caver('https://api.baobab.klaytn.net:8651/');
+  
+    caver.klay.getBlock('latest').then((latestBlock: any) => {
+      console.log(latestBlock)
+      this.setState({latestBlock: caver.utils.hexToNumber(latestBlock.number)})
+    })
+  }
+
+  render() {
+    const latestBlockNumber = this.state.latestBlock;
+
+    return (
+      <View style={styles.container}>
+        <Text>Latest klaytn block number is: {latestBlockNumber}</Text>
+        <Text>Check your console!</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    alignItems: 'flex-start',
     backgroundColor: '#fff',
-    alignItems: 'center',
+    flex: 1,
     justifyContent: 'center',
+    margin: 20,
   },
 });
